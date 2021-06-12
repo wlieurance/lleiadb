fc4c8b8b6a093c28d7d74fb773bbc535c0552432
Wade Lieurance <wlieurance@gmail.com>
2021-06-11 17:47:58 -0700

    added automatic COMMENTing on tables and views from source xml metadata

--- create_db.R
+++ create_db.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
-libraries = c("DBI", "dplyr", "getPass", "optparse", "pool", 
-              "readr", "RPostgres", "sf", "stringr")
+libraries = c("DBI", "dplyr", "getPass", "glue", "optparse", "pool", 
+              "readr", "RPostgres", "sf", "stringr", "XML")
 
 for (lib in libraries){
   suppressMessages(library(lib, character.only = TRUE))
@@ -191,6 +191,140 @@ create.spatial <- function(spatial.path){
 }
 
 
+#' An SQL execution function which uses a try-catch statement to skip/print 
+#' errors if found in the comment SQL. 
+#'
+#' @param sql character string. The SQL to execute. 
+#'
+#' @return Nothing.
+#' @export
+execute.comment <- function(sql){
+  res <- tryCatch(
+    expr = {
+      dbExecute(pool, sql)
+    },
+    error = function(e){
+      print(e)
+      cat(paste0("FAILED: ", sql, " skipping...\n\n"))
+    })
+}
+
+#' Creates comment SQL.
+#'
+#' @param tbl.type character string. The type of comment to be created. One of 
+#'   c("table", "view", "column")
+#' @param schema character string. The name of the schema on which to comment.
+#' @param name character string. The name of the table or view on which to 
+#'   comment.
+#' @param def character string. The comment text.
+#' @param col character string. The name of the column on which to comment. 
+#'   Can be NULL to make a table or view comment.
+#'
+#' @return character string. An SQL statement that can be executed by the 
+#'   database.
+#' @export
+create.comment.sql <- function(tbl.type, schema, name, def, col = NULL){
+  type <- str_to_upper(tbl.type)
+  name.frmt <- paste0('"', name, '"')
+  if (!is.null(col)){
+    col.frmt <- paste0('"', col, '"')
+  } else {
+    col.frmt <- col
+  }
+  def.frmt <- def %>% 
+    str_replace_all("&#60;|&lt;", "<") %>%
+    str_replace_all("&#62;|&gt;", ">") %>%
+    str_replace_all("&#38;|&amp;", "&") %>%
+    str_replace_all("&#39;|&apos;|'", "''") %>%
+    str_replace_all("&#34;|&quot;", '"')
+  fullname <- paste(c(schema, name.frmt, col.frmt), collapse = ".")
+  sql <- glue("COMMENT ON {type} {fullname} IS '{def.frmt}';")
+  return(sql)
+}
+
+
+#' Main processing function for inserting iso19110-2016 catalog metadata in XML
+#' format into the database as comments.
+#'
+#' @param xml.path A character string. Path to the xml metadata for the database.
+#'
+#' @return Nothing.
+#' @export
+comment.from.xml <- function(xml.path){
+  cat("Writing table/view and field COMMENTs to database from metadata...\n")
+  data <- xmlParse(xml.path)
+  xml_data <- xmlToList(data)
+  
+  # extract xml FC_FeatureType and FC_FeatureAttribute to list
+  # and check for xlink mismatches in xml.
+  comment.list <- list()
+  i <- 0  # a counter to keep track of iterations in the outer loop
+  for (tag in xml_data){
+    i <-  i + 1
+    if (class(tag) == "list"){
+      if (!is.null(tag$FC_FeatureType)){
+        name <- tag$FC_FeatureType$typeName
+        def <- tag$FC_FeatureType$definition$CharacterString
+        abstract <- tag$FC_FeatureType$isAbstract$Boolean
+        if (abstract == "false"){
+          type <- "table"
+        } else {
+          type <- "view"
+        }
+        id <- tag$FC_FeatureType$.attrs[["id"]]
+        if (id != name){
+          print(paste0("mismatch: id: ", id, " name: ", name))
+        }
+        j <- 0  # a counter to keep track of iterations in the inner loop
+        field.list <- list()
+        for (tag2 in tag$FC_FeatureType){
+          j <-  j + 1
+          if (!is.null(tag2) & class(tag2) == "list"){
+            if (!is.null(tag2$FC_FeatureAttribute)){
+              if (!is.null(tag2$FC_FeatureAttribute$featureType)){
+                link <- tag2$FC_FeatureAttribute$featureType[["href"]]
+                memname <- tag2$FC_FeatureAttribute$memberName
+                memdef <- tag2$FC_FeatureAttribute$definition$CharacterString
+              }
+              if (paste0("#", id) != link){
+                print(paste0("in table ", id, ": xlink mismatch: ", link,
+                             " for memberName: ", memname))
+              }
+              field.sub <- list(type="column", name=memname, def=memdef)
+              field.list[[length(field.list) + 1]] <- field.sub
+            }
+          }
+        }
+        comment.sub <- list(type=type, name=name, def=def, fields=field.list)
+        comment.list[[length(comment.list) + 1]] <- comment.sub
+      }
+    }
+  } 
+  
+  # construct comment sql for each table/view and column within.
+  sql.v = character()
+  for (comment in comment.list){
+    element <- str_split(comment$name, "\\.")[[1]]
+    schema <- element[1]
+    tbl <- element[2]
+    sql <- create.comment.sql(tbl.type = comment$type, schema = schema, 
+                              name = tbl, def = comment$def)
+    # print(sql)
+    sql.v <- c(sql.v, sql)
+    for (field in comment$fields){
+      sql.f <- create.comment.sql(tbl.type = field$type, schema = schema, 
+                                  name = tbl, col = field$name, def = field$def)
+      # print(sql.f)
+      sql.v <- c(sql.v, sql.f)
+    }
+  }
+  
+  # execute the comment sql
+  for (sql in sql.v){
+    execute.comment(sql)
+  } 
+}
+
 #' The main processing function.
 #'
 #' @param dbname A string. The database name to connect to in the postgres 
@@ -221,6 +355,7 @@ main <-  function(dbname, user, password, sql.path, spatial.path,
   create.eco(sql.path)
   create.spatial(spatial.path)
   create.public(sql.path)
+  comment.from.xml(xml.path = "./metadata/lleiadb_metadata_iso19110-2016.xml")
 
   poolClose(pool)
   rm(pool, envir = .GlobalEnv)
