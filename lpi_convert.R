e29f3e193771a3ecd5a5ec5d3e76b814252a132f
Wade Lieurance <wlieurance@gmail.com>
2023-09-07 15:16:01 -0700

    made the input parameters more pliable for easier and more intuitive(?) use

--- lpi_convert.R
+++ lpi_convert.R
@@ -19,36 +19,33 @@ options(readr.show_progress = FALSE)
 #' case of multiple filters, uses dplyr::bind_rows() to combine.
 #'
 #' @param in.tbl A tibble. Produced by the lpi_calc module and imported.
-#' @param filter.df A tibble with the character fields: \code{field},
+#' @param indicator.tbl A tibble with the character fields: \code{field},
 #'   \code{indicator}, and \code{hit_type}. The \code{field} column must contain
 #'   one of \code{c("cover", "height", "dead")}. The indicator field must match
-#'   indicators to filter \code{in.tbl} by or NA, which will result in all
-#'   indicators of a certain \code{hit_type} being selected. The same logic
-#'   applies to the \code{hit_type} field.
+#'   indicators to filter \code{in.tbl} field \code{indicator}, and the hit_type
+#'   field must be similarly populated to match \code{in.tbl} field
+#'   \code{hit_type}, generally one of (any, all, top, lower, surface,
+#'   herbaceous, woody).
 #' @param sd Boolean. Use standard deviation field(s) in wide result.
 #' @param n Boolean. Use line count (n) field(s) in wide result.
 #'
 #' @return A tibble which has been pivoted to a wide format via
 #'   tidyr::pivot_longer() where each row has a unique plotkey and survey_year
 #' @importFrom foreach %do%
-filter.widen <- function(in.tbl, filter.df, sd = TRUE, n = TRUE){
+filter.widen <- function(in.tbl, indicator.tbl, sd = TRUE, n = TRUE, 
+                         simplify = FALSE){
   static.cols <- c("plotkey", "survey_year", "survey_date_min")
-  vals <- unique(filter.df$field)
+  vals <- unique(indicator.tbl$field)
   values.from <- character()
   values.fill <- list()
 
-  filt.df.grp <- filter.df |> dplyr::group_by(indicator, hit_type) |>
+  filt.df.grp <- indicator.tbl |> dplyr::group_by(indicator, hit_type) |>
     dplyr::summarize(n = dplyr::n(), .groups = "drop")
 
-  filt.select <- filter.df |>
-    dplyr::mutate(m = paste0("^", paste(
-      dplyr::case_when(is.na(indicator) ~ "[a-z_]+?",
-                TRUE ~ indicator),
-      dplyr::case_when(is.na(hit_type) ~ "[a-z]+?",
-                TRUE ~ hit_type),
-      gsub("cover", "cvr", field),
-                sep = "_")
-      ))
+  filt.select <- indicator.tbl |>
+    dplyr::mutate(m = paste0("^", paste(indicator, hit_type, 
+                                        gsub("cover", "cvr", field), 
+                                        sep = "_")))
   if ("cover" %in% vals){
     values.from <- c(values.from, "cvr_pct_mean")
     values.fill[["cvr_pct_mean"]] = 0
@@ -85,19 +82,11 @@ filter.widen <- function(in.tbl, filter.df, sd = TRUE, n = TRUE){
       values.fill[["dead_n"]] = 0
       }
   }
-  # fix for no vals (empty)
-  filt.df <- foreach::foreach(i = as.integer(rownames(filt.df.grp)),
-                              .combine = dplyr::bind_rows) %do% {
-    ind <- filt.df.grp[i,]$indicator
-    ht <- filt.df.grp[i,]$hit_type
-    if(!is.na(ind) & !is.na(ht)){
-      in.tbl |> dplyr::filter(indicator == ind, hit_type == ht)
-    } else if (!is.na(ind)){
-      in.tbl |> dplyr::filter(indicator == ind)
-    } else if (!is.na(ht)){
-      in.tbl |> dplyr::filter(hit_type == ht)
-    }
-  }
+
+  filt.df <- in.tbl |> 
+    dplyr::inner_join(filt.df.grp, 
+                      by = c("indicator" = "indicator", 
+                             "hit_type" = "hit_type"))
 
   filt.df.distinct <- filt.df |> dplyr::distinct()
 
@@ -106,7 +95,8 @@ filter.widen <- function(in.tbl, filter.df, sd = TRUE, n = TRUE){
                 names_from = c(indicator, hit_type),
                 values_from = tidyselect::all_of(values.from),
                 names_glue = "{indicator}_{hit_type}_{.value}",
-                values_fill = values.fill, names_sort = TRUE)
+                values_fill = values.fill,
+                names_sort = TRUE)
 
   filt.df.sel <- filt.df.wide |>
     dplyr::select(tidyselect::matches(c(static.cols, filt.select$m),
@@ -118,7 +108,17 @@ filter.widen <- function(in.tbl, filter.df, sd = TRUE, n = TRUE){
   filt.df.sort <- filt.df.sel |> dplyr::select(
     tidyselect::all_of(c(static.cols, dynamic.cols))
     )
-  return(filt.df.sort)
+
+ # repair our columns names so they aren't all wacky
+  repaired.cols <- dynamic.cols |>
+    stringr::str_replace_all("[^A-Za-z_\\d]", "") |>
+    stringr::str_replace("^([^A-Za-z])", "n\\1") |>
+    stringr::str_to_lower() |> make.unique(sep = "_")
+  names(repaired.cols) <- dynamic.cols
+  filt.repair <- filt.df.sort |>
+    dplyr::rename_with(~repaired.cols)
+
+  return(filt.repair)
 }
 
 #' Takes a tibble produced from filter.table(), joins site data and point data
@@ -215,8 +215,8 @@ lpi.to.gis <- function(
     in.tbl <- tibble::as_tibble(readr::read_delim(in.path, delim = sep,
                                                   show_col_types = FALSE))
   }
-
-  in.wide <- filter.widen(in.tbl = in.tbl, filter.df = filter.df,
+  indicator.tbl <- create.indicator.tbl(filter.df = filter.df, in.tbl = in.tbl)
+  in.wide <- filter.widen(in.tbl = in.tbl, indicator.tbl = indicator.tbl,
                           sd = sd, n = n)
 
 
@@ -230,48 +230,138 @@ lpi.to.gis <- function(
 #' Creates a filter.df tibble to be used in \code{lpi.to.gis} from strings.
 #' Primarily used for command line purposes.
 #'
-#' @param cover_str character string. A semicolon delimited string of comma
+#' @param cover_hit character string. A comma delimited string of
+#'   \code{hit_type} that gives preference order for \code{hit_type} when
+#'   selecting records from a \code{calc.lpi} output which will be used to
+#'   create a long -> wide sf data frame for export using cover fields
+#'   (cvr_*). Only one \code{hit_type} will be used per indicator and all
+#'   indicators will be selected
+#' @param height_hit character string. A comma delimited string 
+#'   (see cover_filter) for creating height fields (height_*) based on 
+#'   \code{hit_type} preference order.
+#' @param dead_hit character string. A comma delimited string 
+#'   (see cover_filter) for creating height fields (dead_*) based on 
+#'   \code{hit_type} preference order.
+#' @param cover_filter character string. A semicolon delimited string of comma
 #'   delimited pairs consisting of \code{indicator} and \code{hit_type} that
 #'   should be selected from a \code{calc.lpi} output which will be used to
 #'   create a long -> wide sf data frame for export using cover fields
-#'   (cvr_*).
-#' @param height_str character string. A semicolon delimited string of comma
-#'   delimited pairs consisting of \code{indicator} and \code{hit_type} that
-#'   should be selected from a \code{calc.lpi} output which will be used to
-#'   create a long -> wide sf data frame for export using height fields
-#'   (height_*).
-#' @param dead_str character string. A semicolon delimited string of comma
-#'   delimited pairs consisting of \code{indicator} and \code{hit_type} that
-#'   should be selected from a \code{calc.lpi} output which will be used to
-#'   create a long -> wide sf data frame for export using dead fields
-#'   (dead_*).
+#'   (cvr_*). Mutually exclusive with \code{cover_hit}.
+#' @param height_filter character string. A semicolon delimited string of comma
+#'   delimited pairs (see cover_filter) for creating height fields (height_*).
+#'   Mutually exclusive with \code{height_hit}.
+#' @param dead_filter character string. A semicolon delimited string of comma
+#'   delimited pairs (see cover_filter) for creating dead fields (dead_*).
+#'   Mutually exclusive with \code{dead_hit}.
 #'
 #' @return a tibble
 #' @export
 #'
-#' @examples create.filter.df(cover_str = "indicator1, any; indicator2, top")
-create.filter.df <- function(cover_str = NULL, height_str = NULL,
-                             dead_str = NULL){
-  filter.str <- c(cover = cover_str,
-                  height = height_str,
-                  dead = dead_str)
-  # pattern = r"{([^,;\s]+)[\s,]*([^,;\s]+)*[\s;$]*}"
+#' @examples create.filter.df(height_hit = "any, all", cover_filter =
+#'   "indicator1, any; indicator2, top; indicator 3")
+create.filter.df <- function(cover_hit = NULL, height_hit = NULL,
+                             dead_hit = NULL, cover_filter = NULL,
+                             height_filter = NULL, dead_filter = NULL) {
+  # sanity check
+  if (!is.null(cover_hit) && !is.null(cover_filter)) {
+    cat(paste("cover_hit and cover_filter are mutually exclusive, please give",
+              "only one of these options.\n"))
+    quit()
+  }
+  if (!is.null(height_hit) && !is.null(height_filter)) {
+    cat(paste("height_hit and height_filter are mutually exclusive, please",
+              "give only one of these options.\n"))
+    quit()
+  }
+  if (!is.null(dead_hit) && !is.null(dead_filter)) {
+    cat(paste("dead_hit and dead_filter are mutually exclusive, please give",
+              "only one of these options.\n"))
+    quit()
+  }
+
+  filter.df <- tibble::tibble(field = character(), indicator = character(),
+                              hit_type = character(), rank = integer())
+  # specific filters
+  filter.str <- c(cover = cover_filter,
+                  height = height_filter,
+                  dead = dead_filter)
   split.1 <- stringr::str_split(filter.str, "\\s*;\\s*")
   filt.1 <- lapply(X = split.1, FUN = function (x) x[which(trimws(x) != "")])
   split.2 <- lapply(X = filt.1, FUN = function (x)
     stringr::str_split(x, "\\s*,\\s*"))
   names(split.2) <- names(filter.str)
-  filter.df <- tibble::tibble(field = character(), indicator = character(),
-                              hit_type = character())
   for (cat in names(split.2)){
     for (row in split.2[[cat]]){
       filter.df <- filter.df |>
-        tibble::add_row(field = cat, indicator = row[1], hit_type = row[2])
+        tibble::add_row(field = cat, indicator = row[1], hit_type = row[2],
+                        rank = 1)
     }
   }
+
+  # hit filters
+  hit.str <- c(cover = cover_hit,
+               height = height_hit,
+               dead = dead_hit)
+  hit.split <- sapply(X = hit.str, FUN = function (x)
+    stringr::str_split(x, "\\s*,\\s*"))
+  for (cat in names(hit.split)){
+    rank <- 1
+    for (row in hit.split[[cat]]){
+      filter.df <- filter.df |>
+        tibble::add_row(field = cat, indicator = NA, hit_type = row[1],
+                        rank = rank)
+      rank <- rank + 1 
+    }
+  }
+
   return(filter.df)
 }
 
+
+#' Creates a table of indicator and hit_type combos for each field (cover,
+#' height, dead) from what is actually in the data compared to what has been
+#' created with create.filter.df()
+#'
+#' @param filter.df A tibble produced by create.filter.df() or having the same
+#'   definition.
+#' @param in.tbl A tibble. Produced by the lpi_calc module and imported.
+#' @return A tibble with a unique combination of indicators and hit_types which
+#'   can be used to filter the main data table.
+#' @export
+create.indicator.tbl <- function(filter.df, in.tbl){
+  indicators <- in.tbl |> dplyr::group_by(indicator, hit_type) |>
+    dplyr::summarize(.groups = "drop")
+  fields <- filter.df |> dplyr::group_by(field) |> 
+    dplyr::summarize(.groups = "drop") |> dplyr::pull(field)
+
+  ind.tbl <- foreach::foreach (f = fields, .combine = dplyr::bind_rows) %do% {
+    filter.grp <- filter.df |> dplyr::filter(field == f) |>
+      dplyr::group_by(indicator, hit_type, rank) |>
+      dplyr::summarize(.groups = "drop")
+    specific.ind <- filter.grp |> 
+      dplyr::filter((!is.na(indicator) & !is.na(hit_type)))
+    general.ind <- filter.grp |> dplyr::filter(is.na(hit_type)) |>
+      dplyr::select(indicator, rank) 
+    general.ht <- filter.grp |> dplyr::filter(is.na(indicator)) |>
+      dplyr::select(hit_type, rank)
+    ind.combo <- indicators |> 
+      dplyr::inner_join(general.ind, by = c("indicator" = "indicator"))
+    hit.combo <- indicators |> 
+      dplyr::inner_join(general.ht, by = c("hit_type" = "hit_type"))
+    combo <- dplyr::bind_rows(ind.combo, hit.combo, specific.ind)
+    combo.min.rank <- combo |> dplyr::group_by(indicator) |> 
+      dplyr::summarize(min_rank = min(rank), .groups = "drop")
+    combo.filt <- combo |> 
+      dplyr::inner_join(combo.min.rank, 
+                        by = c("indicator" = "indicator", 
+                               "rank" = "min_rank")) |>
+      dplyr::mutate(field = f)
+
+    combo.filt
+  }
+  ind.unique <- ind.tbl |> dplyr::select(-rank) |> dplyr::distinct()
+  return(ind.unique)
+}
 # Run if module is called from Rscript.
 if (sys.nframe() == 0) {
   args = commandArgs(trailingOnly = TRUE)
@@ -293,20 +383,41 @@ if (sys.nframe() == 0) {
                     r"{case of escaped characters (e.g. \t) you must}",
                     " pass the literal character recongnized your ",
                     r"{shell (e.g. $'\t' for bash).}")),
-    optparse::make_option(opt_str = c("-c", "--cover"),
+    optparse::make_option(opt_str = c("--cover_hit"),
+                help = paste0("string which restricts output fields to a ",
+                              "specific hit type for all indicators (1 per ",
+                              "indicator) in order of preference. format: ",
+                              "\"hit_type1, hit_type2, etc.\". where ",
+                              "hit type is one of (any, all, top, lower, ",
+                              "surface, herbaceous, woody). If the first ",
+                              "hit_type is unavailable for an indicator, the ",
+                              "second given is then used and so on. This ",
+                              "option is mutually exculsive with the ",
+                              "--cover_filter option.")),
+    optparse::make_option(opt_str = c("--height_hit"),
+                help = paste0("string which restricts output fields to a",
+                              "specific hit_type for all height indicators ",
+                              "(mutually exclusive with --height_filter, see ",
+                              "--cover_hit).")),
+    optparse::make_option(opt_str = c("--dead_hit"),
+                help = paste0("string which restricts output fields to a",
+                              "specific hit_type for all dead indicators ",
+                              "(mutually exclusive with --dead_filter, see ",
+                              "--cover_hit).")),
+    optparse::make_option(opt_str = c("--cover_filter"),
                 help = paste0("string which restricts output fields to ",
                               "specific cover indicators using the following ",
                               "format: \"indicator_name1, hit_type1; ",
                               "indicator_name2, hit_type2; etc.\", where ",
                               "hit type is either blank or one of (any, all, ",
-                              "top, lower, surface, herbaceous, woody, all)")),
-    optparse::make_option(opt_str = c("-t", "--height"),
+                              "top, lower, surface, herbaceous, woody)")),
+    optparse::make_option(opt_str = c("--height_filter"),
                 help = paste0("string which restricts output fields to ",
                               "specific height indicators.")),
-    optparse::make_option(opt_str = c("-d", "--dead"),
+    optparse::make_option(opt_str = c("--dead_filter"),
                 help = paste0("string which restricts output fields to ",
                               "specific dead indicators.")),
-    optparse::make_option(opt_str = c("-D", "--sd"), action = "store_true",
+    optparse::make_option(opt_str = c("--sd"), action = "store_true",
                           default = FALSE,
                           help = paste0("Include standard deviation ",
                                         "calculations for each field.")),
@@ -332,9 +443,12 @@ if (sys.nframe() == 0) {
     opt$options$password = getPass::getPass()
   }
   # convert our string command line inputs to lists
-  f.df <- create.filter.df(cover_str = opt$options$cover,
-                           height_str = opt$options$height,
-                           dead_str = opt$options$dead)
+  f.df <- create.filter.df(cover_hit = opt$options$cover_hit,
+                           height_hit = opt$options$height_hit,
+                           dead_hit = opt$options$dead_hit,
+                           cover_filter = opt$options$cover_filter,
+                           height_filter = opt$options$height_filter,
+                           dead_filter = opt$options$dead_filter)
 
   lpi.to.gis(
     dbname = opt$args[1], host = opt$options$host, port = opt$options$port,
