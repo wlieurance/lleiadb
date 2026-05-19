72807a3951466c556ccbfe5fb4f28c647b0d1f14
Wade Lieurance <wlieurance@gmail.com>
2026-05-19 11:41:40 -0700

    added functionality to allow creation of new databases if they dont exist using a maintenance db

--- common.R
+++ common.R
@@ -11,32 +11,38 @@
 #' @return A DBI connection object
 #' @export
 connect_pg <- function(dbname, host = "localhost", port = 5432, user = NULL,
-                       password = NULL) {
+                       password = NULL, maintenance_db = NULL,
+                       allow_create = FALSE, recurse = 0) {
 
+  if (recurse > 4) {
+    stop("Something went wrong... stopping function recursion...")
+  }
   if (is.null(user)) {
     user <- Sys.info()[["user"]]
   }
 
-  try_connect <- function(password = NULL) {
-    args <- list(
-      drv = RPostgres::Postgres(),
-      dbname = dbname,
-      host = host,
-      port = port,
-      user = user
-    )
+  if (is.null(maintenance_db)) {
+    maintenance_db <- user
+  }
 
-    if (!is.null(password)) {
-      args$password <- password
-    }
+  args <- list(
+    drv = RPostgres::Postgres(),
+    dbname = dbname,
+    host = host,
+    port = port,
+    user = user
+  )
 
-    do.call(DBI::dbConnect, args)
+  if (!is.null(password)) {
+    args$password <- password
   }
 
   tryCatch(
-    try_connect(password),
+    do.call(DBI::dbConnect, args),
     error = function(e) {
       msg <- conditionMessage(e)
+
+      # print(msg)
 
       needs_password <- grepl(
         "no password supplied|password authentication failed",
@@ -44,15 +50,47 @@ connect_pg <- function(dbname, host = "localhost", port = 5432, user = NULL,
         ignore.case = TRUE
       )
 
-      if (!needs_password) {
+      needs_db <- grepl(
+        paste0('database "', dbname, '" does not exist'),
+        msg,
+        fixed = TRUE
+      )
+
+      if (!needs_password && !needs_db) {
         stop(e)
       }
 
-      password <- getPass::getPass(
-        paste0("Password for PostgreSQL user '", user, "': ")
-      )
+      if (needs_password) {
+        password <- getPass::getPass(
+          paste0("Password for PostgreSQL user '", user, "': ")
+        )
+        args$password <- password
+      }
 
-      try_connect(password)
+      if (needs_db) {
+        if (allow_create) {
+          message(paste0('Creating database "', dbname, '"...'))
+          new_args <- args
+          new_args$dbname <- maintenance_db
+          con <- do.call(DBI::dbConnect, new_args)
+          on.exit(DBI::dbDisconnect(con), add = TRUE)
+          DBI::dbExecute(
+            con,
+            paste("CREATE DATABASE", DBI::dbQuoteIdentifier(con, dbname))
+          )
+        } else {
+          stop(
+            paste0('Database "', dbname, '" does not exist and creation ',
+                   "disallowed. Stopping.."),
+            call. = FALSE  # cleaner error msg w/o function call
+          )
+        }
+      }
+
+      connect_pg(dbname = dbname, host = host, port = port,
+                 user = user, password = args$password,
+                 maintenance_db = maintenance_db, allow_create = allow_create,
+                 recurse = recurse + 1)
     }
   )
 }
